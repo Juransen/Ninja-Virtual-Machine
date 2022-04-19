225aa3a3b18c78a07384db20f5188d900d9ca4bd
Jurasen <juergen.schwitzt@gmial.com>
2022-04-19 15:35:07 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/KSP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KSP.dir/hello.c.o"
+  "KSP"
+  "KSP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/KSP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Ninja Virtual Machine started\n");
+
+    printf("Ninja Virtual Machine stopped\n");
+    return 0;
+}
