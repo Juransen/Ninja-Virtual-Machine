164bc3c6302c9e9c503dd88fd5d6ef761ec98324
Jurasen <juergen.schwitzt@gmial.com>
2022-05-22 22:06:21 +0200

    aufgabe 0 fertig

--- _njvm.c
+++ _njvm.c
@@ -14,14 +14,12 @@ void commandResponse (char* incomeCommand[], int arraySize){
 
     int cmdType = -2;
 
-    //printf("%d\n", arraySize);
-
     if(arraySize > 1) {
         cmdType = -1;
 
         for (int i = 0; i < sizeof(commands) / sizeof(char *); i++) {
-            int cmdCmp = strcmp(commands[i], incomeCommand[1]);
 
+            int cmdCmp = strcmp(commands[i], incomeCommand[1]);
             if (cmdCmp == 0) { cmdType = i; }
         }
     }
@@ -29,27 +27,23 @@ void commandResponse (char* incomeCommand[], int arraySize){
     switch (cmdType) {
 
         default: {
-            //printf("default\n");
             printf("Ninja Virtual Machine started\n");
             printf("Ninja Virtual Machine stopped\n");
             break;
         }
 
         case -1:{
-            //printf("-1  invalid command\n");
             printf("unknown command line argument \x1B[31m'%s'\033[0m, try \x1B[31m'./njvm --help'\033[0m\n", incomeCommand[1]);
             break;
         }
 
         case 0: {
-            //printf("0 help\n");
             printf("usage: ./njvm [option] [option] ...\n--version\tshow version and exit\n"
                    "--help\t\tshow this help and exit\n");
             break;
         }
 
         case 1: {
-            //printf("1 version\n");
             printf("Ninja Virtual Machine version %i (compiled %s, %s)\n",buildVersion, buildDate, buildTime);
             break;
 
@@ -57,9 +51,6 @@ void commandResponse (char* incomeCommand[], int arraySize){
 
     }
 }
-
-
-
 
 int main(int argc, char *argv[]) {
 
