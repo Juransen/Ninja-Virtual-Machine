f9e079b16ebafd57de5bde524431346546d2b5b5
Jurasen <juergen.schwitzt@gmial.com>
2022-05-22 14:34:54 +0200

    21.05.

--- _njvm.c
+++ _njvm.c
@@ -3,15 +3,39 @@
 
 const char* commands[] = {"--help", "--version"};
 
-void cmdOutput(int x){
-    printf("lol");
+void commandResponse (char* incomeCommand[]){
+
+    int cmdType = -1;
+
+    int arraySize = sizeof(incomeCommand) / sizeof(char*);
+
+    if(arraySize > 1) {
+
+        for (int i = 0; i < sizeof(commands) / sizeof(char *); i++) {
+            int cmdCmp = strcmp(commands[i], incomeCommand[1]);
+
+            if (cmdCmp == 0) { cmdType = i; }
+
+        }
+    }
+
+
+    switch (cmdType) {
+        case -1: {
+            printf("-1\n");
+        }
+        case 0: {
+            printf("0\n");
+        }
+        case 1: {
+            printf("1\n");
+        }
+
+    }
 }
-// seg fault liegt an speicher der entweder nicht zugeteilt ist oder keine permission für den Speicher
 
-
-int main(int argc, char *argv[]) {
-
-    for (int i = 1; i < argc; i++) {
+/*
+ *  for (int i = 1; i < argc; i++) {
 
         for(int j = 0; j < sizeof(commands) / sizeof(char*); j++){  //expresion in methode auslagern
 
@@ -24,6 +48,16 @@ int main(int argc, char *argv[]) {
             }
         }
     }
+ *
+ */
+
+
+// seg fault liegt an speicher der entweder nicht zugeteilt ist oder keine permission für den Speicher
+
+
+int main(int argc, char *argv[]) {
+
+    commandResponse(argv);
 
     printf("Ninja Virtual Machine started\n");
     printf("Ninja Virtual Machine stopped\n");
